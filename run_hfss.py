3857a7e621ae07d5f027387c8d10eb2d2c83e623
Summers, Randall J <rsummers@ou.edu>
2020-07-07 11:46:10 -0500

    Send a message after HFSS exits.

--- run_hfss.py
+++ run_hfss.py
@@ -1,3 +1,22 @@
 import subprocess
+import json
+import smtplib
+from email.message import EmailMessage
 
-subprocess.run([r'C:\Program Files\AnsysEM\AnsysEM19.4\Win64\ansysedt.exe', "-RunScriptAndExit", "analyze_design.py"])
+subprocess.run([r'C:\Program Files\AnsysEM\AnsysEM19.4\Win64\ansysedt.exe',
+                "-RunScriptAndExit", "analyze_design.py"])
+
+with open("secrets.json") as f:
+    email_conf = json.load(f)
+
+msg = EmailMessage()
+msg['From'] = email_conf["from"]
+msg["To"] = email_conf["to"]
+msg['Subject'] = "HFSS Simulation"
+email_content = "Your HFSS Simulation recently completed."
+msg.set_content(email_content)
+
+
+server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+server.login(email_conf["from"], email_conf["pass"])
+server.send_message(msg)
